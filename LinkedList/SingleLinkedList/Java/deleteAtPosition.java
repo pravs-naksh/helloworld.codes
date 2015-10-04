4b71838d51ade6ab213a819ad956dbe283f966ab
pravs-naksh <pravs.naksh@gmail.com>
2015-10-04 16:02:01 -0400

    Create deleteAtPosition methodfor SingleLinkedList

---
+++ LinkedList/SingleLinkedList/Java/deleteAtPosition.java
@@ -0,0 +1,31 @@
+public boolean deleteAtPosition(int position) {
+		if (position == 0){
+			return deleteFront();
+		}
+		if (position == this.length()-1){
+			return deleteBack();
+		}
+		
+		if(this.head == null)
+			return false;
+		else{
+			if(head.getNext()==null){
+				head = null;
+				return true;
+			} else {
+				Node current = this.head;
+				Node previous = this.head;
+				int counter = 0;
+				while(current.getNext()!=null){
+					if(counter==position){
+						previous.setNext(current.getNext());
+						return true;
+					}
+					previous = current;
+					current = current.getNext();
+					counter++;
+				}
+				return false;
+			}
+		}
+}
