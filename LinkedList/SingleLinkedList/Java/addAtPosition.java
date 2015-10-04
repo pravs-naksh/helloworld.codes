38910ba206ac87827d02f9ee54b0192c5d92b674
pravs-naksh <pravs.naksh@gmail.com>
2015-10-04 15:16:55 -0400

    Create addAtPosition for SIngleLinkedList

---
+++ LinkedList/SingleLinkedList/Java/addAtPosition.java
@@ -0,0 +1,30 @@
+public void addAtPosition(Object data, int position) {
+		if (position == 0){
+			addFront(data);
+			return;
+		}
+		if (position >= this.length()){
+			addBack(data);
+			return;
+		}
+
+		Node newNode = new Node(data);
+		if (this.head == null) {
+			this.head = newNode;
+		} else {
+			Node current = this.head;
+			Node previous = this.head;
+			int counter = 0;
+			while (current != null) {
+				if (counter == position) {
+					previous.setNext(newNode);
+					newNode.setNext(current);
+					break;
+				}
+				previous = current;
+				current = current.getNext();
+				counter++;
+			}
+
+		}
+}
