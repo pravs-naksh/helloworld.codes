14863958b69af6c3d2b2d4aa5e00c5456ac56bfd
pravs-naksh <pravs.naksh@gmail.com>
2015-10-08 00:18:41 -0400

    Create addAtPosition for DoublyLinkedList

---
+++ LinkedList/DoublyLinkedList/Java/addAtPosition.java
@@ -0,0 +1,33 @@
+public void addAtPosition(Object data, int position) {
+		if (position == 0){
+			addFront(data);
+			return;
+		}
+		if (position >= this.length()){
+			addBack(data);
+			return;
+		}
+
+		Node newNode = new Node(data);
+		if (this.head == null && this.tail == null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			Node current = this.head;
+			Node previous = this.head;
+			int counter = 0;
+			while (current != null) {
+				if (counter == position) {
+					previous.setNext(newNode);
+					newNode.setPrev(previous);
+					newNode.setNext(current);
+					current.setPrev(newNode);
+					break;
+				}
+				previous = current;
+				current = current.getNext();
+				counter++;
+			}
+
+		}
+}
