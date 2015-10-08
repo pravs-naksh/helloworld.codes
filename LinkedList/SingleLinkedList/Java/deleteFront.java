53eb93a6bff79c015f46cb2ac45cf7e5b8080047
pravs-naksh <pravs.naksh@gmail.com>
2015-10-08 00:23:11 -0400

    Update deleteFront.java

--- LinkedList/SingleLinkedList/Java/deleteFront.java
+++ LinkedList/SingleLinkedList/Java/deleteFront.java
@@ -1,9 +1,10 @@
 public boolean deleteFront() {
-		if(this.head == null)
+		if(this.head == null && this.tail == null)
 			return false;
 		else{
 			if(head.getNext()==null){
 				head = null;
+				tail = null;
 				return true;
 			} else {
 				head = head.getNext();
