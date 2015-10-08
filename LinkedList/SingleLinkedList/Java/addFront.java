5f571dde36692ed2b8d6ba05248f88e748db84e9
pravs-naksh <pravs.naksh@gmail.com>
2015-10-08 00:05:28 -0400

    Adding tail to the design
    If this is used as a queue then we need the insertion to be O(1)

--- LinkedList/SingleLinkedList/Java/addFront.java
+++ LinkedList/SingleLinkedList/Java/addFront.java
@@ -2,6 +2,7 @@ public void addFront(Object data) {
 		Node newNode = new Node(data);
 		if(this.head==null){
 			this.head = newNode;
+			this.tail = newNode;
 		} else {
 			Node tempHead = this.head;
 			newNode.setNext(tempHead);
