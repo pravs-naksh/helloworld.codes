390e1087a2c09b743d79390db5d49fe82f18268a
pravs-naksh <pravs.naksh@gmail.com>
2015-10-08 00:36:15 -0400

    Create deleteAtPosition for DoublyLinkedList

---
+++ LinkedList/DoublyLinkedList/Java/deleteAtPosition.java
@@ -0,0 +1,33 @@
+public boolean deleteAtPosition(int position) {
+		if (position == 0){
+			return deleteFront();
+		}
+		if (position == this.length()-1){
+			return deleteBack();
+		}
+		
+		if(this.head == null && this.tail == null)
+			return false;
+		else{
+			if(head.getNext()==null){
+				head = null;
+				tail = null;
+				return true;
+			} else {
+				Node current = this.head;
+				Node previous = this.head;
+				int counter = 0;
+				while(current.getNext()!=null){
+					if(counter==position){
+						previous.setNext(current.getNext());
+						current.getNext().setPrev(previous);
+						return true;
+					}
+					previous = current;
+					current = current.getNext();
+					counter++;
+				}
+				return false;
+			}
+		}
+	}
