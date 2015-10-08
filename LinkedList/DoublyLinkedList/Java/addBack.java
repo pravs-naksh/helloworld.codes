1475445cf228cc54582457d6f340aeeb8891f031
pravs-naksh <pravs.naksh@gmail.com>
2015-10-08 00:15:14 -0400

    Create addBack for DoublyLinkedList

---
+++ LinkedList/DoublyLinkedList/Java/addBack.java
@@ -0,0 +1,12 @@
+public void addBack(Object data) {
+		Node newNode = new Node(data);
+		if (this.head == null && this.tail == null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			Node tempTail = this.tail;
+			tempTail.setNext(newNode);
+			newNode.setPrev(tempTail);
+			this.tail = newNode;
+		}
+}
