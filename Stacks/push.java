4fc66ff904e56965963252d720ca405482bf267c
pravs-naksh <pravs.naksh@gmail.com>
2015-10-09 01:49:18 -0400

    Update push.java

--- Stacks/push.java
+++ Stacks/push.java
@@ -1,5 +1,11 @@
 public void push(Object data){
   //Invoking addFront from Single/Doubly Linked List Implementation
-  this.internalSingleLL.addFront(data);
-  this.top = this.internalSingleLL.getFront();
+  if (this.size == this.capacity) {
+		System.out.println("Stack full!");
+		return;
+	}
+	
+	this.linkedlist.addFront(data);
+	this.top = this.linkedlist.getFront();
+	this.setSize(getSize() + 1);
 }
